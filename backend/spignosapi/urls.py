3c5a5dd3c308217d4ff7b573cf7f4fe42561bdf0
Martin <martin.paul.feldmann@gmail.com>
2025-03-16 00:35:31 +0100

    added home template and preparing for api technical roadmap

--- backend/spignosapi/urls.py
+++ backend/spignosapi/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .views import home
 
 urlpatterns = [
+    path('', home, name='home'),  # Définition de la route pour /
     path('admin/', admin.site.urls),
-    path('api/', include('spignosapi.api.urls')),  # Inclure les endpoints de l’API
+    path('api/', include('spignosapi.api.urls')),
 ]
