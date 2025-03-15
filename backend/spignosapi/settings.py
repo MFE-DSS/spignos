baf007f6c316f21a53e968d3fe4c0393996c168a
Martin <martin.paul.feldmann@gmail.com>
2025-03-15 23:54:31 +0100

    first deployment succeed

--- backend/spignosapi/settings.py
+++ backend/spignosapi/settings.py
@@ -66,7 +66,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'api.middleware.CustomErrorMiddleware'
+    'spignosapi.api.middleware.CustomErrorMiddleware'
 ]
 
 ROOT_URLCONF = 'spignosapi.urls'
