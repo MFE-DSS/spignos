2d99779778e834113966e7bdaab666c33ccabf02
Martin <martin.paul.feldmann@gmail.com>
2025-04-13 15:03:38 +0200

    fix flake8 error : CI - SPIGNOS #71

--- backend/spignosapi/llm_handler.py
+++ backend/spignosapi/llm_handler.py
@@ -1,10 +1,12 @@
 from transformers import pipeline, AutoTokenizer, AutoModelForCausalLM
 
+
 MODEL_PATH = "/absolute/path/to/PycharmProjects/spignos/local_models/mistral"
 
 tokenizer = AutoTokenizer.from_pretrained(MODEL_PATH)
 model = AutoModelForCausalLM.from_pretrained(MODEL_PATH)
 llm_pipeline = pipeline("text-generation", model="./llm_models/mistral", local_files_only=True)
+
 
 def generate_response(prompt: str) -> str:
     response = llm_pipeline(prompt, max_new_tokens=100, do_sample=True)
